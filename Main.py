bf9f1617582b233e17dc97682db01343941608c8
Vivian Nguyen <vivian.nguyen1127@gmail.com>
2021-01-30 14:02:18 -0800

    nguyev12 - Reformat files for modularity

---
+++ Main.py
@@ -0,0 +1,8 @@
+import ExtractText
+import ParseExtractedText
+import PopulateParkingLocations
+
+
+PopulateParkingLocations.post_lot()
+ExtractText.detect_text('test-5.jpg')
+ParseExtractedText.calculate_density('Lot6A')
