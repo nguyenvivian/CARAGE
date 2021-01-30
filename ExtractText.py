d5cb57d3ff6b0744de229d1e7cb391d699d447d6
Vivian Nguyen <vivian.nguyen1127@gmail.com>
2021-01-29 23:55:47 -0800

    nguyev12 - POST request to store location and phrases in Firebase

--- ExtractText.py
+++ ExtractText.py
@@ -1,10 +1,15 @@
 import io
 import os
 from google.cloud import vision
+from firebase import firebase
+from datetime import datetime
 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'carage-service-account-token-f525563457d6.json'
+firebase = firebase.FirebaseApplication('https://formal-era-303305-default-rtdb.firebaseio.com/', None)
 
 
+# Takes the path to an image and sends a request to the Vision API.
+# Image is then parsed for text and returned via a JSON response.
 def detect_text(path):
     client = vision.ImageAnnotatorClient()
 
@@ -27,6 +32,17 @@ def detect_text(path):
     if response.error.message:
         raise Exception('{}\n'.format(response.error.message))
 
+    post_text(extractedPhrases)
+
     return extractedPhrases
+
+
+# Sends a POST request to the Firebase Realtime DB.
+def post_text(extractedPhrases):
+    data = {'ParkingLot': 'Lot 6A',
+            'TimeTaken': datetime.now(),
+            'ExtractedPhrasesRaw': extractedPhrases
+            }
+    result = firebase.post('/ParkingLocation/Lot6A/', data)
 
 # detect_text('test-4.jpg')
